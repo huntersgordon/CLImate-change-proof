4af973426d40433c4de238b6df37f1e2cbd24c9f
Hunter Gordon <Hunter@airbears2-10-142-176-241.airbears2.1918.berkeley.edu>
2017-08-30 17:08:49 -0700

    CLImate change is real

---
+++ CLImate_change.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+     string yourname, yourfriendsname,yourmate,yourfriendsmate;
+     cout<<"Enter your name: ";
+     cin >> yourname;
+     cout<<"Enter your friend's name: ";
+     cin >>yourfriendsname;
+     cout<<"Enter your mate: ";
+     cin >> yourmate;
+     cout<<"Enter your friend's mate: ";
+     cin >> yourfriendsmate;
+
+     cout<<"Your new mate is: " + yourfriendsmate<<endl;
+     cout<<"Your friend's new mate is: " + yourmate <<endl;
+     cout<<"CLI mate change is real."<<endl;
+     cout << "Press Enter to Continue";
+     getchar();
+     getchar(); 
+
+
+
+
+
+
+}
